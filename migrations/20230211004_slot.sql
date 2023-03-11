df22f3fe44460ca1530452951d8d0a9ee097df00
jirayu <jirayu.ka@logicspark.com>
2023-03-12 00:10:34 +0700

    create slot.sql

---
+++ migrations/20230211004_slot.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS "parking"."slots";
+
+CREATE SEQUENCE "parking".slot_id_seq
+	START WITH 1
+	INCREMENT BY 1
+	NO MINVALUE
+	NO MAXVALUE
+	CACHE 1;
+CREATE TABLE IF NOT EXISTS "parking"."slots" (
+    id BIGINT DEFAULT nextval('"parking".slot_id_seq'::regclass),
+    "floorId" INT,
+    "slotNumber" INT,
+    "isSlotAvailable" BOOLEAN DEFAULT false,
+    "active" BOOLEAN DEFAULT true,
+    "deleted" BOOLEAN DEFAULT false,
+    "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    "createdBy" CHARACTER VARYING,
+    "updatedAt" TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    "updatedBy" CHARACTER VARYING,
+
+    CONSTRAINT "pk_slots" PRIMARY KEY ("id"),
+    CONSTRAINT "fk_slots_floor" FOREIGN KEY ("floorId") REFERENCES "parking"."floors"(id)
+);
