7f5171c85ee235bdb9449ad92ae3e60485ef0df7
jirayu <jirayu.ka@logicspark.com>
2023-03-11 23:31:24 +0700

    create parking-lot.sql

---
+++ migrations/20230211001_parking-lot.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS "parking"."parking_lot";
+CREATE SEQUENCE "parking".parking_lot_id_seq
+	START WITH 1
+	INCREMENT BY 1
+	NO MINVALUE
+	NO MAXVALUE
+	CACHE 1;
+CREATE TABLE IF NOT EXISTS "parking"."parking_lot" (
+    id BIGINT DEFAULT nextval('"parking".parking_lot_id_seq'::regclass),
+    "name" CHARACTER VARYING NULL,
+    "number_of_blocks" INT,
+    "active" BOOLEAN DEFAULT true,
+    "deleted" BOOLEAN DEFAULT false,
+    "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    "createdBy" CHARACTER VARYING,
+    "updatedAt" TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    "updatedBy" CHARACTER VARYING,
+
+    CONSTRAINT "pk_parking_lot" PRIMARY KEY ("id")
+);
