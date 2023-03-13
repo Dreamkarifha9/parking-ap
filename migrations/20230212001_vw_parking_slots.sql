6c063d55b5e68521fa12a57a326d5f48f351a42f
jirayu <jirayu.ka@logicspark.com>
2023-03-13 16:09:42 +0700

    fix vw_parking_slot.sql

--- migrations/20230212001_vw_parking_slots.sql
+++ migrations/20230212001_vw_parking_slots.sql
@@ -1,27 +1,31 @@
 DROP VIEW IF EXISTS "parking".vw_parking_slots;
-CREATE OR REPLACE VIEW "parking".vw_parking_slots AS
-SELECT 
-  pl."id" as "parkingLotId",
-  pl.name as "parkingName",
-  b."id" as "blockId",
-  b."blockCode",    
-  b."blockSize",
-  f."id" as "floorId",
-  f."floorNumber",
-  f."numberOfSlot",
-  psr."numberPlate",
-  psr."carSize",
-  s."id" as "slotId",
-  s."slotNumber",
-  s."isAvailable" AS "slotIsAvailable",
-  s."active",
-  s."deleted",
-  s."createdAt" AS "createdAt",
-  s."createdBy" AS "createdBy",
-  s."updatedAt" AS "updatedAt",
-  s."updatedBy" AS "updatedBy"
-FROM parking."slots" s
-  LEFT JOIN parking.floors f ON f.id = s."floorId" and f.active = true and f.deleted = false
-  left join parking.blocks b on b.id = f."blockId" and b.active = true and b.deleted = false
-  left join parking.parking_lot pl on pl.id = b."parkingLotId" and pl.active = true and pl.deleted = false
-  left join parking.parking_slot_reservations psr on psr."slotId" = s.id and psr.active = true and psr.deleted = false;
+CREATE OR REPLACE VIEW parking.vw_parking_slots
+AS SELECT pl.id AS "parkingLotId",
+    pl.name AS "parkingName",
+    b.id AS "blockId",
+    b."blockCode",
+    b."blockSize",
+    f.id AS "floorId",
+    f."floorNumber",
+    f."numberOfSlot",
+    case when s."isAvailable" = true then
+    (select psr."numberPlate" from parking.parking_slot_reservations psr where psr."slotId" = s.id order by "createdAt" desc limit 1)
+    else null
+    end as "numberPlate",
+        case when s."isAvailable" = true then
+    (select psr."carSize" from parking.parking_slot_reservations psr where psr."slotId" = s.id order by "createdAt" desc limit 1)
+    else null
+    end as "carSize",
+    s.id AS "slotId",
+    s."slotNumber",
+    s."isAvailable" AS "slotIsAvailable",
+    s.active,
+    s.deleted,
+    s."createdAt",
+    s."createdBy",
+    s."updatedAt",
+    s."updatedBy"
+   FROM parking.slots s
+     LEFT JOIN parking.floors f ON f.id = s."floorId" AND f.active = true AND f.deleted = false
+     LEFT JOIN parking.blocks b ON b.id = f."blockId" AND b.active = true AND b.deleted = false
+     LEFT JOIN parking.parking_lot pl ON pl.id = b."parkingLotId" AND pl.active = true AND pl.deleted = false;
