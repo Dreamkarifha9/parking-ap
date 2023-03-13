436cd4b1b8e38941a4bb7478f2c533f4a99a9e06
jirayu <jirayu.ka@logicspark.com>
2023-03-14 00:15:07 +0700

    :white_check_mark:

--- src/parking-slot-reservation/parking-slot-reservation.service.spec.ts
+++ src/parking-slot-reservation/parking-slot-reservation.service.spec.ts
@@ -7,24 +7,47 @@ import { VWParkingSlotsService } from '../slots/vw-parking-slots.service';
 import { SlotsService } from '../slots/slots.service';
 import { VWParkingSlot } from '../slots/entities/vw-parking-slot.entity';
 import { Slot } from '../slots/entities/slot.entity';
-import { addMilliseconds } from 'date-fns'
+import { addMilliseconds } from 'date-fns';
 import { Repository } from 'typeorm';
 import { CreateParkingSlotReservationDto } from './dto/create-parking-slot-reservation.dto';
 import { ECarSize } from '../shared/enums';
 
-
 describe('ParkingSlotReservationService', () => {
   let parkingSlotReservationService: ParkingSlotReservationService;
-  let parkingSlotReservationRepository: Repository<ParkingSlotReservation>
+  let parkingSlotReservationRepository: Repository<ParkingSlotReservation>;
   const parkingSlotReservationEntityList: Partial<ParkingSlotReservation>[] = [
-    { slotId: 1, startTimestamp: addMilliseconds(new Date(), 1), exitTimestamp: addMilliseconds(new Date(), 1), durationInMinutes: 10, bookingDate: addMilliseconds(new Date(), 1), numberPlate: 'กท.1111', carSize: ECarSize.SMAILL },
-    { slotId: 1, startTimestamp: addMilliseconds(new Date(), 2), exitTimestamp: addMilliseconds(new Date(), 2), durationInMinutes: 10, bookingDate: addMilliseconds(new Date(), 2), numberPlate: 'กท.2222', carSize: ECarSize.SMAILL },
-    { slotId: 1, startTimestamp: addMilliseconds(new Date(), 3), exitTimestamp: addMilliseconds(new Date(), 3), durationInMinutes: 10, bookingDate: addMilliseconds(new Date(), 3), numberPlate: 'กท.3333', carSize: ECarSize.SMAILL }
+    {
+      slotId: 1,
+      startTimestamp: addMilliseconds(new Date(), 1),
+      exitTimestamp: addMilliseconds(new Date(), 1),
+      durationInMinutes: 10,
+      bookingDate: addMilliseconds(new Date(), 1),
+      numberPlate: 'กท.1111',
+      carSize: ECarSize.SMAILL,
+    },
+    {
+      slotId: 1,
+      startTimestamp: addMilliseconds(new Date(), 2),
+      exitTimestamp: addMilliseconds(new Date(), 2),
+      durationInMinutes: 10,
+      bookingDate: addMilliseconds(new Date(), 2),
+      numberPlate: 'กท.2222',
+      carSize: ECarSize.SMAILL,
+    },
+    {
+      slotId: 1,
+      startTimestamp: addMilliseconds(new Date(), 3),
+      exitTimestamp: addMilliseconds(new Date(), 3),
+      durationInMinutes: 10,
+      bookingDate: addMilliseconds(new Date(), 3),
+      numberPlate: 'กท.3333',
+      carSize: ECarSize.SMAILL,
+    },
   ];
 
   const mockCreateParkingSlotReservation = {
-    suscuess: true
-  }
+    suscuess: true,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -38,28 +61,32 @@ describe('ParkingSlotReservationService', () => {
             checkIn: jest.fn(),
             checkOut: jest.fn(),
             findOneBySearch: jest.fn(),
-            findAll: jest.fn().mockResolvedValue(parkingSlotReservationEntityList),
+            findAll: jest
+              .fn()
+              .mockResolvedValue(parkingSlotReservationEntityList),
             findOneByNumberPlate: jest.fn(),
             update: jest.fn(),
-
-          }
+          },
         },
         {
           provide: getRepositoryToken(VWParkingSlot),
           useValue: {
             findOneBySearch: jest.fn(),
-          }
+          },
         },
         {
           provide: getRepositoryToken(Slot),
-          useValue: {}
-        }
-
+          useValue: {},
+        },
       ],
     }).compile();
 
-    parkingSlotReservationService = module.get<ParkingSlotReservationService>(ParkingSlotReservationService);
-    parkingSlotReservationRepository = module.get<Repository<ParkingSlotReservation>>(getRepositoryToken(ParkingSlotReservation));
+    parkingSlotReservationService = module.get<ParkingSlotReservationService>(
+      ParkingSlotReservationService,
+    );
+    parkingSlotReservationRepository = module.get<
+      Repository<ParkingSlotReservation>
+    >(getRepositoryToken(ParkingSlotReservation));
   });
 
   it('should be defined', () => {
@@ -70,11 +97,11 @@ describe('ParkingSlotReservationService', () => {
     it('check in successfully', async () => {
       const data: CreateParkingSlotReservationDto = {
         numberPlate: 'กท.1111',
-        carSize: ECarSize.SMAILL
-      }
-      const reuslt = await parkingSlotReservationService.checkIn(data)
+        carSize: ECarSize.SMAILL,
+      };
+      const reuslt = await parkingSlotReservationService.checkIn(data);
 
-      expect(reuslt).toEqual({ suscuess: true })
+      expect(reuslt).toEqual({ suscuess: true });
     });
-  })
+  });
 });
